a894558620f4e8e9fdd3ebe72bb6e12508e6af52
jasmine Magdi <jesojolay@yahoo.com>
2017-09-09 12:44:25 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,37 @@
 chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(arr, element) {
+    return arr[element,...arr];
+}
+
+function destructivelyAddElementToBeginningOfArray(arrm element) {
+    arr.unshift(element);
+}
+
+function addElementToEndOfArray() {
+
+}
+
+function destructivelyAddElementToEndOfArray() {
+
+}
+
+function accessElementInArray() {
+    
+}
+
+function destructivelyRemoveElementFromBeginningOfArray () {
+    
+}
+
+function removeElementFromBeginningOfArray () {
+    
+}
+
+function destructivelyRemoveElementFromEndOfArray () {
+    
+}
+
+function removeElementFromEndOfArray () {
+    
+}
